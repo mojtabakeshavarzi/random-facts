39765ef397fa75e04f8550f58090ad26a060e2ad
Mojtaba Keshavarzi <mojtabakeshavarzi1998@gmail.com>
2023-02-15 17:33:53 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import data from './data';
 
 function App() {
+
+  const[count , setCount] = useState(0);
+  const[text , setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if(count <= 0) {
+      amount = 1 
+    }
+    if (count > 4) {
+      amount = 4 
+    }
+    setText(data.slice(0,amount));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section-center">
+      <h4>Random Facts</h4>
+      <form className="lorem" onSubmit={handleSubmit}>
+        <label htmlFor="total">number of paragraphs : </label>
+
+        <input 
+        type="number" 
+        name="total"
+        value={count}
+        onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit">Generate</button>
+    <article className="lorem-text">
+      {text.map((item) => {
+        return <p>{item}</p>
+      })}
+    </article>
+      </form>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,11 @@
+const data = [
+    `The human heart creates enough pressure to squirt blood 30 feet (9 m). Diet Coke was only invented in 1982. There are more than 1,700 references to gems and precious stones in the King James translation of the Bible. When snakes are born with two heads, they fight each other for food.`,
+    `The half-hour series premiered on December 17, 1989, with "Simpsons Roasting on an Open Fire", a Christmas special. "Some Enchanted Evening" was the first full-length episode produced, but it did not broadcast until May 1990, as the last episode of the first season, because of animation problems.
+    `,
+    `Whales are mammals who have no sweat glands, but then since they live in the water, they don't really need them.
+    `,
+    `It is not true, as is commonly believed, that bulls have horns and cows do not: the presence of horns depends on the breed, or in horned breeds on whether the horns have been disbudded (conversely, in many breeds of sheep it is indeed only the males which have horns).
+    `
+];
+
+export default data;
